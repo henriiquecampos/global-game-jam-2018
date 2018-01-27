635cf679d2c8497c4fad7e234a31e34c1b985839
Henrique Campos <henriiquecampos@gmail.com>
2018-01-26 23:42:03 -0200

    Added collision check for player
    Also delegated player check for player scene's root node

--- source/actors/player/input.gd
+++ source/actors/player/input.gd
@@ -1,14 +1,13 @@
 extends Node
 
 onready var parent = get_parent()
-export (String, "P1", "P2") var player = "P1"
 
 func _physics_process(delta):
-	if Input.is_action_pressed(player + "_UP"):
+	if Input.is_action_pressed(parent.player + "_UP"):
 		parent.movement(Vector2(0, -1))
-	elif Input.is_action_pressed(player + "_DOWN"):
+	elif Input.is_action_pressed(parent.player + "_DOWN"):
 		parent.movement(Vector2(0, 1))
-	if Input.is_action_pressed(player + "_RIGHT"):
+	if Input.is_action_pressed(parent.player + "_RIGHT"):
 		parent.movement(Vector2(1, 0))
-	elif Input.is_action_pressed(player + "_LEFT"):
+	elif Input.is_action_pressed(parent.player + "_LEFT"):
 		parent.movement(Vector2(-1, 0))
--- source/actors/player/player.gd
+++ source/actors/player/player.gd
@@ -1,7 +1,13 @@
 extends KinematicBody2D
 export (float) var speed = 100
+export (String, "P1", "P2") var player = "P1"
 
 func movement(dir):
 	move_and_collide(dir * speed * get_physics_process_delta_time())
 	
-func dominance(
+func dominance(tower):
+	tower.set_player(player)
+
+func _on_body_entered( area ):
+	if area.is_in_group("tower"):
+		dominance(area.get_parent())
