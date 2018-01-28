7245cc222ebc03d9765f5d48df5312928badcaf7
Henrique Campos <henriiquecampos@gmail.com>
2018-01-28 12:13:41 -0200

    Added tutoring

---
+++ source/screens/main_screen/main_screen.gd
@@ -0,0 +1,18 @@
+extends "res://screens/abstract_screen.gd"
+
+onready var bgm_bus = AudioServer.get_bus_index(bgm.get_bus())
+#onready var sfx_bus = AudioServer.get_bus_index(sfx.get_bus())
+
+func _ready():
+	$VolumeBGM.set_value(AudioServer.get_bus_volume_db(bgm_bus))
+#	$VolumeSFX.set_value(AudioServer.get_bus_volume_db(sfx_bus))
+
+func _on_bgm_value_changed( value ):
+	AudioServer.set_bus_volume_db(bgm_bus, value)
+
+func _on_sfx_value_changed( value ):
+#	AudioServer.set_bus_volume_db(sfx_bus, value)
+	pass
+
+func quit():
+	get_tree().quit()
